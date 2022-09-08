e6d46267db89b9376fa6c38372bfa520862e9799
Dafahan <dafahanid@gmail.com>
2022-09-08 09:29:50 +0700

    Add files via upload

---
+++ main (15).cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+#define FOR(a,b,c) for(int a=b;c;a++)
+#define RFOR(a,b,c) for(int a=b;c;a--)
+#define all(a) a.begin(),a.end()
+#define sz size()
+#define elif else if
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define pb push_back
+#define br cout<<endl;
+using namespace std;
+
+int n,k;
+string s;
+signed main(){
+   cin>>n;
+   cout<<4*n<<' '<<pow(n,2);
+   return 0;
+}
+
